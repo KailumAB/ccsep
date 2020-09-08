9c50db969fa973620790a00e1df04629cc168316
Kailum Bull <kailum.a.bull@gmail.com>
2020-09-08 22:07:05 +0800

    Add faith's site for testing

---
+++ src/.flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=vulnerable_app.py
+FLASK_ENV=development
---
+++ src/config.py
@@ -0,0 +1,14 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    # This key is supposed to prevent CSRF attacks which you are not being tested
+    # on for this lab. Please ignore it
+    SECRET_KEY = "this-is-a-secret-key"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:password@172.16.0.2:3306/demodb"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_HTTPONLY = True
+    REMEMBER_COOKIE_SECURE = True
+    REMEMBER_COOKIE_HTTPONLY = True
