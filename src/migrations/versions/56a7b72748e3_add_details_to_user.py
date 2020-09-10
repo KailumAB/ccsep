285b57e4524e28bc55f3c3617ae2fc2e6282c6ad
Kailum Bull <kailum.a.bull@gmail.com>
2020-09-10 22:03:10 +0800

    Add more fields to user

---
+++ src/migrations/versions/56a7b72748e3_add_details_to_user.py
@@ -0,0 +1,24 @@
+"""Add details to user
+
+Revision ID: 56a7b72748e3
+Revises: 508d8dfd7e99
+Create Date: 2020-09-10 09:35:45.152471
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '56a7b72748e3'
+down_revision = '508d8dfd7e99'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('user', sa.Column('dob', sa.DateTime))
+    op.add_column('user', sa.Column('postcode', sa.Integer()))
+
+def downgrade():
+    pass
