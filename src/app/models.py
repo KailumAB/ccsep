f7feb94e6f838ac435fc172ade8e1f81dd8f10b4
Kailum Bull <kailum.a.bull@gmail.com>
2020-09-14 16:31:05 +0800

    Set password to length 300

--- src/app/models.py
+++ src/app/models.py
@@ -12,16 +12,17 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password = db.Column(db.String(64))
+    password = db.Column(db.String(300))
     dob = db.Column(db.DateTime)
     postcode = db.Column(db.Integer)
     __searchable__ = ["username"]
 
     def set_password(self, password):
-        self.password = password
+        self.password = bcrypt.encrypt(password)
 
     def check_password(self, password):
-        return self.password == password
+        return bcrypt.verify(password, self.password)
+
 
     def __repr__(self):
         return "<User {}>".format(self.username)
--- src/migrations/versions/5d4231b8b67a_users_table.py
+++ src/migrations/versions/5d4231b8b67a_users_table.py
@@ -22,7 +22,7 @@ def upgrade():
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('username', sa.String(length=64), nullable=False),
     sa.Column('email', sa.String(length=120), nullable=False),
-    sa.Column('password', sa.String(length=64), nullable=False),
+    sa.Column('password', sa.String(length=300), nullable=False),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_index(op.f('ix_user_email'), 'user', ['email'], unique=True)
